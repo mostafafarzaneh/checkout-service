c7cc3affcd993e8debdb203b9b0a5f32dcfc37cf
Mostafa Farzaneh <mostafa.farzaneh@jibb.ai>
2025-02-24 10:30:58 +1100

    feat: Add HTTP handler and server

---
+++ cmd/server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"log"
+
+	"checkout/internal/application"
+	"checkout/internal/infrastructure/repository"
+	"checkout/internal/interface/http/handlers"
+)
+
+func main() {
+	repo := repository.NewDefaultMemoryDB()
+	checkout := application.NewCheckout(repo)
+
+	handler := handlers.NewPurchaseHandler(checkout)
+	http.Handle("/purchase", handler)
+
+	log.Println("Starting server on :8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Server Failed: %v", err)
+	}
+}
---
+++ internal/interface/http/handlers/purchase_handler.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"checkout/internal/domain"
+	"checkout/internal/application"
+)
+
+type PurchaseRequest struct {
+	Items []domain.CartItem `json:"items"`
+}
+
+type PurchaseResponse struct {
+	Invoice domain.Invoice `json:"invoice"`
+	Error string  `json:"error, omitempty"`
+}
+
+type PurchaseHandler struct {
+	Checkout *application.Checkout
+}
+
+func NewPurchaseHandler(checkout *application.Checkout) *PurchaseHandler {
+	return &PurchaseHandler{checkout}
+}
+
+func (h *PurchaseHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req PurchaseRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	invoice, err := h.Checkout.Purchase(req.Items)
+	if err != nil {
+		 log.Printf("Purchase error: %v", err)
+		 resp := PurchaseResponse{
+			 Error: err.Error(),
+			 Invoice: domain.Invoice{},
+		 }
+		 w.WriteHeader(http.StatusConflict)
+		 _ = json.NewEncoder(w).Encode(resp)
+		 return
+	}
+
+	resp := PurchaseResponse{
+		Invoice: invoice,
+	}
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(resp)
+}
